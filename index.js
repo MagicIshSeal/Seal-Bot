4f5db0688dc62aad8a80465744280372d7122039
M Van <85613794+MagicIshSeal@users.noreply.github.com>
2022-03-29 21:14:55 +0000

    You can now roll multiple dice

--- index.js
+++ index.js
@@ -8,22 +8,31 @@ client.once('ready', () => {
 client.on("message", msg =>{
         var mesg = msg.content;
         if(mesg.includes("+roll") === true){
-          var stat = "roll"; 
+          var stat = "roll"; //set msg stat to roll
         }
   
         switch(stat){
           case "roll":
-            var num = dice();
             var split = mesg.split(/(\s+)/);
+            if(split[2] === undefined){
+              for(var i = 0; i < 1; i++){
+              var num = dice();
+              msg.channel.send(num);
+            }
+            }else{
+              for(var i = 0; i < split[2]; i++){
+              var num = dice();
+              msg.channel.send(num);
+            }
+            } 
             function dice(){
               let res = split[0].substring(5)
               let num = Math.ceil(Math.random()*res);
               return num;
             }
-            console.log(split[1])
-            msg.channel.send(num);
-            break;
-        }
+            }
+            
+      
 });
 
 
